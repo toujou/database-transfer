2f8216c1a1f3e2988208e47f29929303acb1f565
Thomas Maroschik <tmaroschik@dfau.de>
2025-01-21 12:00:34 +0100

    test: add a dummy test for unit tests

---
+++ Tests/Unit/DummyTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Toujou\DatabaseTransfer\Tests\Unit;
+
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+class DummyTest extends UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function dummy(): void
+    {
+        self::assertTrue(true);
+    }
+}
