6e847b60962c4742cefd78ece7279b409f0a1c3a
Thomas Maroschik <tmaroschik@dfau.de>
2025-07-29 13:45:27 +0200

    refactor: switch from tca to schema in RelationAnalyzer

--- Classes/Database/RelationAnalyzer.php
+++ Classes/Database/RelationAnalyzer.php
@@ -7,61 +7,44 @@ namespace Toujou\DatabaseTransfer\Database;
 use Doctrine\DBAL\ParameterType;
 use Toujou\DatabaseTransfer\Export\ExportIndex;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Schema\Field\FieldTypeInterface;
+use TYPO3\CMS\Core\Schema\Field\RelationalFieldTypeInterface;
+use TYPO3\CMS\Core\Schema\PassiveRelation;
+use TYPO3\CMS\Core\Schema\RelationshipType;
+use TYPO3\CMS\Core\Schema\TcaSchemaFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class RelationAnalyzer
 {
     private array $relationsQueryCache = [];
 
-    private array $foreignFields = [];
+    private readonly TcaSchemaFactory $schemaFactory;
+
+    private readonly array $backwardsPointingRelations;
 
     public function __construct(
         private readonly ExportIndex $exportIndex,
+        TcaSchemaFactory $schemaFactory = null,
     ) {
-        $this->foreignFields = $this->buildForeignFields();
+        $this->schemaFactory = $schemaFactory ?? GeneralUtility::makeInstance(TcaSchemaFactory::class);
+        $this->backwardsPointingRelations = $this->buildBackwardsPointingRelations();
     }
 
-    private function buildForeignFields(): array
+    private function buildBackwardsPointingRelations(): array
     {
-        $foreignFields = [];
-        $foreignFieldRelationsQuery = $this->exportIndex->getConnection()->createQueryBuilder();
-        $foreignFieldRelationsExpr = $foreignFieldRelationsQuery->expr();
-        $foreignFieldRelationsQuery->getRestrictions()->removeAll();
-        $foreignFieldRelationsQuery->select('ri.tablename', 'ri.field', 'ri.flexpointer', 'ri.ref_table')->from('sys_refindex', 'ri')
-            ->join('ri', $this->exportIndex->getIndexTableName(), 'exr', 'exr.tablename = ri.ref_table AND exr.sourceuid = ri.ref_uid')
-            ->where($foreignFieldRelationsExpr->and(
-                $foreignFieldRelationsExpr->eq('ri.softref_key', $foreignFieldRelationsQuery->quote('')),
-            ))
-            ->groupBy('ri.tablename', 'ri.field', 'ri.flexpointer', 'ri.ref_table');
-        foreach ($foreignFieldRelationsQuery->executeQuery()->iterateAssociative() as $foreignFieldRelation) {
-            if (!isset($GLOBALS['TCA'][$foreignFieldRelation['tablename']]['columns'][$foreignFieldRelation['field']]['config'])) {
-                continue;
+        $backwardsPointingRelations = [];
+        $recordTableNames = $this->exportIndex->getRecordTableNames();
+        foreach ($recordTableNames as $tableName) {
+            $schema = $this->schemaFactory->get($tableName);
+            // We only take care of foreign fields, as all other relations are already covered by the forward pointing relations
+            $foreignFieldFields = $schema->getFields(fn(FieldTypeInterface $field) => $field instanceof RelationalFieldTypeInterface && $field->getRelationshipType() === RelationshipType::OneToMany);
+            foreach ($foreignFieldFields as $foreignFieldField) {
+                foreach ($foreignFieldField->getRelations() as $relation) {
+                    $backwardsPointingRelations[$relation->toTable()][] = new PassiveRelation($tableName, $foreignFieldField->getName(), null);
+                }
             }
-            $columnConfig = $GLOBALS['TCA'][$foreignFieldRelation['tablename']]['columns'][$foreignFieldRelation['field']]['config'];
-            // TODO resolve flexpointers here
-            $foreignFields = $this->addForeignFieldForColumn($columnConfig, $foreignFieldRelation, $foreignFields);
         }
-
-        return $foreignFields;
-    }
-
-    private function addForeignFieldForColumn(array $columnConfig, array $relation, array $foreignFields): array
-    {
-        if (
-            !isset($columnConfig['type'], $columnConfig['foreign_table'], $columnConfig['foreign_field']) ||
-            !\in_array($columnConfig['type'], ['select', 'inline', 'category', 'file'])
-        ) {
-            return $foreignFields;
-        }
-
-        if (!isset($foreignFields[$columnConfig['foreign_table']][$columnConfig['foreign_field']])) {
-            $foreignFields[$columnConfig['foreign_table']][$columnConfig['foreign_field']] = [];
-        }
-        $foreignFields[$columnConfig['foreign_table']][$columnConfig['foreign_field']][] = [
-            'tableName' => $relation['tablename'],
-            'columnName' => $relation['field'],
-        ];
-
-        return $foreignFields;
+        return $backwardsPointingRelations;
     }
 
     public function getRelationsForRecord(string $tableName, int $uid): array
@@ -71,18 +54,18 @@ class RelationAnalyzer
         } else {
             $queries = [$this->buildForwardPointingRelationsQuery()];
 
-            $hasForeignFieldConstraints = count($this->foreignFields[$tableName] ?? []) > 0;
-            if ($hasForeignFieldConstraints) {
-                $queries[] = $this->buildBackwardsPointingRelationsQuery($tableName);
+            $backwardsPointingQuery = $this->buildBackwardsPointingRelationsQuery($tableName);
+            if ($backwardsPointingQuery) {
+                $queries[] = $backwardsPointingQuery;
             }
 
             $sql = \implode(' UNION ', \array_map(fn(QueryBuilder $query) => $query->getSQL(), $queries));
             $preparedQuery = $this->exportIndex->getConnection()->prepare($sql);
 
-            $execQuery = $this->relationsQueryCache[$tableName] = function (string $tableName, int $uid) use ($preparedQuery, $hasForeignFieldConstraints) {
+            $execQuery = $this->relationsQueryCache[$tableName] = function (string $tableName, int $uid) use ($preparedQuery, $backwardsPointingQuery) {
                 $preparedQuery->bindValue(1, $tableName);
                 $preparedQuery->bindValue(2, $uid, ParameterType::INTEGER);
-                if ($hasForeignFieldConstraints) {
+                if ($backwardsPointingQuery) {
                     $preparedQuery->bindValue(3, $tableName);
                     $preparedQuery->bindValue(4, $uid, ParameterType::INTEGER);
                 }
@@ -126,19 +109,22 @@ class RelationAnalyzer
         return $query;
     }
 
-    private function buildBackwardsPointingRelationsQuery(string $tableName): QueryBuilder
+    private function buildBackwardsPointingRelationsQuery(string $tableName): ?QueryBuilder
     {
+        $passiveRelations = $this->backwardsPointingRelations[$tableName] ?? [];
+        if (empty($passiveRelations)) {
+            return null;
+        }
+
         $query = $this->exportIndex->getConnection()->createQueryBuilder();
         $expr = $query->expr();
 
         $foreignFieldsConstraints = [];
-        foreach ($this->foreignFields[$tableName] ?? [] as $foreignFields) {
-            foreach ($foreignFields as $foreignField) {
-                $foreignFieldsConstraints[] = $expr->and(
-                    $expr->eq('ri.tablename', $query->quote($foreignField['tableName'])),
-                    $expr->eq('ri.field', $query->quote($foreignField['columnName'])),
-                );
-            }
+        foreach ($passiveRelations as $passiveRelation) {
+            $foreignFieldsConstraints[] = $expr->and(
+                $expr->eq('ri.tablename', $query->quote($passiveRelation->fromTable())),
+                $expr->eq('ri.field', $query->quote($passiveRelation->fromField())),
+            );
         }
 
         $query
